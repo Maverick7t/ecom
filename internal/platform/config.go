251a51c657f8109e69ae2dd50e23303c1da73292
folder structure added <moinaktarshaikh651@gmail.com>
2026-06-27 23:59:23 +0530

    imports

--- internal/platform/config.go
+++ internal/platform/config.go
@@ -1,1 +1,27 @@
 package platform
+
+type Config struct {
+	AppEnv  string
+	AppPort string
+
+	DatabaseURL            string
+	DBMaxConns             int32
+	DBConnTimeout          int
+	SupabaseURL            string
+	SupabaseServiceRoleKey string
+	SupabaseJWTSecret      string
+
+	NIMAPIKey  string
+	NIMBaseURL string
+
+	R2AccountID     string
+	R2AccessKey     string
+	R2SecretKey     string
+	R2Bucket        string
+	R2PublicBaseURL string
+
+	OTELEndpoint    string
+	OTELServiceName string
+
+	RateLimitRPM int
+}
