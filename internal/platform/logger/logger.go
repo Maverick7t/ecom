411f674c639d50223f9662fb3b6859dcc9527a43
folder structure added <moinaktarshaikh651@gmail.com>
2026-07-01 13:58:51 +0530

    add environment-aware structured logger configuration

--- internal/platform/logger/logger.go
+++ internal/platform/logger/logger.go
@@ -1,8 +1,24 @@
 package platform
 
+import (
+	"log/slog"
+	"os"
+)
+
 type contextKey string
 
 const (
 	TraceIdKey contextKey = "trace_id"
 	UserIDKey  contextKey = "user_id"
 )
+
+func NewLogger(cfg *Config) *slog.Logger {
+	opts := &slog.HandlerOptions{Level: slog.LevelInfo}
+
+	if cfg.IsDev() {
+		opts.Level = slog.LevelDebug
+		return slog.New(slog.NewTextHandler(os.Stdout, opts))
+	}
+
+	return slog.New(slog.NewJSONHandler(os.Stdout, opts))
+}
