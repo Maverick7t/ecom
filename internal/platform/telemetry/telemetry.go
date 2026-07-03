8ee0dde52f0f5dcdee3859c50bb8b28f2da2cb5b
folder structure added <moinaktarshaikh651@gmail.com>
2026-07-03 22:58:59 +0530

    tracer provider

--- internal/platform/telemetry/telemetry.go
+++ internal/platform/telemetry/telemetry.go
@@ -1,11 +1,29 @@
 package platform
 
 import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptrachttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
 type Telemetry struct {
 	TraceProvider *sdktrace.TracerProvider
-	Tracer        trace.Tracer
+	Tracer 		  trace.Tracer
 }
+
+func NewTelemetry(ctx context.Context, cfg *Config, logger *slog.Logger) (*Telemetry, error) {
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceName(cfg.OTELServiceName),
+			semconv.DeploymentEnviourment(cfg.AppEnv),
+		),
+	)
+		))
