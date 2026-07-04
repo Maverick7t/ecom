054f7ff7897f90d191d08ba91e73b3c9cbe153b6
folder structure added <moinaktarshaikh651@gmail.com>
2026-07-04 14:32:13 +0530

    add request ID middleware

---
+++ internal/api/middleware/middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/Maverick7t/ecom/internal/platform"
+	"github.com/google/uuid"
+)
+
+func RequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *htttp.Request) {
+		traceID := r.Header.Get("x-Request-Id")
+		if traceID == "" {
+			traceID = uuid.New().String()
+		}
+		ctx := platform.WithTraceID(r.Context(), traceID)
+		w.Header().Set("x-Request-ID", traceID)
+		next.ServeHTTP(w, r.WithCOntext(ctx))
+	})
+}
