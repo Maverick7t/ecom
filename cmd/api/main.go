e4c3421cad0f20b2e8445d167a0d9f8c2e1b973c
folder structure added <moinaktarshaikh651@gmail.com>
2026-07-05 22:03:41 +0530

    initialize database and configure http sercer

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/Maverick7t/ecom/internal/api"
 	"github.com/YOURUSERNAME/product-intelligence/internal/platform"
 )
 
@@ -20,6 +23,21 @@ func main() {
 	if err != nil {
 		logger.Error("telemetry error", slog.Any("error", err))
 		os.Exit(1)
+	}
+
+	db, err := platform.NewDB(ctx, cfg, logger)
+	if err != nil {
+		logger.Error("database error", slog.Any("error", err))
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	srv := &http.Server{
+		Addr:         ":" + cfg.AppPort,
+		Handler:      api.NewRouter(cfg, db, logger),
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 
 }
