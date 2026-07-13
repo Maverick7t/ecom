34394883b008bc43f75b9387ed3ed2c593336346
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-13 14:22:35 +0530

    initialize catalog igestion worker

--- internal/jobs/catalog/catalog_ingestion.go
+++ internal/jobs/catalog/catalog_ingestion.go
@@ -22,22 +22,22 @@ import (
 )
 
 type CatalogIngestionJob struct {
-	SourcePath string 'json:"source_path"'
-	Category string 'json:"category"'
-	Limit int 'json':"limit"'
+	SourcePath string `json:"source_path"`
+	Category string `json:"category"`
+	Limit int `json:"limit"`
+}
 
-
-func (CatalogIngestionArgs) King() string { return "catalog_ingestion"}
+func (CatalogIngestionArgs) Kind() string { return "catalog_ingestion"}
 type metadataRecord struct {
-	MainCategory string 'json:"main_category"'
-	Categories []string 'json:"categories"'
-	Title string 'json:"title"'
-	Store string 'json:"store"'
-	Description []string 'json:"description"'
-	Price *string 'json:"price"'
-	ParentAsin string 'json:"parent_asin"'
+	MainCategory string `json:"main_category"`
+	Categories []string `json:"categories"`
+	Title string `json:"title"`
+	Store string `json:"store"`
+	Description []string `json:"description"`
+	Price *string `json:"price"`
+	ParentAsin string `json:"parent_asin"`
 	Images []struct {
-		Large string 'json:"large"'
+		Large string `json:"large"`
 	} 'json:"images"'
 }
 
@@ -55,3 +55,31 @@ func NewWorker(pool *pgxpool.Pool, queries *dbgen.Queries, logger *slog.Logger) 
 		logger:  logger,
 	}
 }
+
+const checkpontInterval = 1000
+
+func (w *Worker) Work(ctx context.Context, job *river.Job[CatalogIngestionArgs]) error {
+	args := job.Args
+	if strings.TrimSpace(args.Category) == "" {
+		return fmt.Errorf("catalog_ingestion: category is required")
+	}
+	if args.Limit <= 0 {
+		args.Limit = 50000
+	}
+ 
+	f, err := os.Open(args.SourcePath)
+	if err != nil {
+		return fmt.Errorf("open source file %s: %w", args.SourcePath, err)
+	}
+	defer f.Close()
+ 
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		return fmt.Errorf("open gzip reader: %w", err)
+	}
+	defer gz.Close()
+ 
+	syncRunID, err := w.queries.CreateSyncRun(ctx, "catalog_ingestion")
+	if err != nil {
+		return fmt.Errorf("create sync_run: %w", err)
+	}
